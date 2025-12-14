cba1c432abf0b815548bf6bfdd23c58e1946830e
Daniel Cepeda <dcep93@gmail.com>
2025-12-14 03:40:50 -0500

    x

--- app/public/customMiddleware.js
+++ app/public/customMiddleware.js
@@ -1,1 +1,19 @@
-alert(2);
+function customMiddleware() {
+  const app = document.getElementById("app");
+  const iframe = document.createElement("iframe");
+  iframe.src = new URL(document.currentScript.src).origin;
+  iframe.hidden = true;
+  app.prepend(iframe);
+
+  function sendMessage(payload) {
+    iframe.contentWindow.postMessage(payload, "*");
+  }
+
+  window.addEventListener("message", (event) => {
+    if (event.data.source === "react-devtools-content-script") return;
+
+    const f = eval(event.data.functionString);
+    f(event.data.payload, iframe, sendMessage);
+  });
+}
+customMiddleware();
--- app/src/tfmars420/index.tsx
+++ app/src/tfmars420/index.tsx
@@ -1,5 +1,73 @@
+import parserBabel from "prettier/plugins/babel";
+import parserEstree from "prettier/plugins/estree";
+import prettier from "prettier/standalone";
+
+import { useEffect, useState } from "react";
 import recorded_sha from "./recorded_sha";
 
-export default function tfmars420() {
-  return <div>{recorded_sha}</div>;
+export default function Tfmars420() {
+  const [functionString, setFunctionString] = useState<string | null>(null);
+  const executePayload = (payload: any) =>
+    sendMessageToParent(functionString!, payload);
+  useEffect(() => {
+    fToString(remoteF)
+      .then((functionString) => {
+        const lines = functionString.split("\n");
+        lines[0] = `${lines[0].match(/\(.*\)/)![0]} => {`;
+        return lines.join("\n");
+      })
+      .then((_functionString) => {
+        setFunctionString(_functionString);
+        return _functionString;
+      });
+  }, []);
+
+  return functionString === null ? null : (
+    <Helper executePayload={executePayload} />
+  );
+}
+
+function Helper(props: { executePayload: (payload: any) => void }) {
+  init(props.executePayload);
+  return (
+    <div onClick={() => props.executePayload({ x: 21 })}>{recorded_sha}</div>
+  );
+}
+
+var initialized = false;
+function init(executePayload: (payload: any) => void) {
+  if (initialized) return;
+  initialized = true;
+  executePayload({ type: "init" });
+}
+
+function sendMessageToParent(functionString: string, payload: any) {
+  window.parent.postMessage({ functionString, payload }, "*");
+}
+
+const fToString = (fn: (...args: any[]) => any): Promise<string> => {
+  return prettier.format(fn.toString(), {
+    parser: "babel",
+    plugins: [parserEstree, parserBabel],
+  });
+};
+
+//
+
+window.addEventListener("message", (event) => {
+  const payload = event.data;
+  alert(JSON.stringify(payload));
+});
+
+function remoteF(
+  payload: any,
+  iFrame: HTMLIFrameElement,
+  _sendMessageToChild: (payload: any) => null
+) {
+  if (payload.type === "init") {
+    iFrame.style.width = "100vW";
+    iFrame.style.height = "10em";
+    iFrame.style.border = "0";
+    iFrame.hidden = false;
+  }
 }
